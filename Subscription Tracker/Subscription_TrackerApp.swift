c2132d2afb59a2e5407be2bbde73bc692b951d55
Nikita Shyshkin <yatoenough@gmail.com>
2024-11-28 16:46:55 +0100

    Initial commit

---
+++ Subscription Tracker/Subscription_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Subscription_TrackerApp.swift
+//  Subscription Tracker
+//
+//  Created by Nikita Shyshkin on 28/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Subscription_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
