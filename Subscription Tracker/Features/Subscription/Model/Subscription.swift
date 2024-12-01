fb6f2ed6cce6c2eb2ad80c4ab6cf67fc2f1ef113
Nikita Shyshkin <yatoenough@gmail.com>
2024-12-01 13:18:27 +0100

    Added subscription model

---
+++ Subscription Tracker/Features/Subscription/Model/Subscription.swift
@@ -0,0 +1,42 @@
+//
+//  Subscription.swift
+//  Subscription Tracker
+//
+//  Created by Nikita Shyshkin on 01/12/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct Subscription: Identifiable {
+    let id: Int
+    let name: String
+    let price: Double
+    let image: String
+    let type: SubscriptionType
+}
+
+struct SubscriptionType {
+    let value: String
+    let color: Color
+}
+
+enum SubscriptionTypes {
+    case yearly
+    case monthly
+    case weekly
+    case threeMonth
+    
+    func getType() -> SubscriptionType {
+        switch self {
+        case .yearly:
+            return SubscriptionType(value: "Yearly", color: .purple)
+        case .monthly:
+            return SubscriptionType(value: "Monthly", color: .green)
+        case .weekly:
+            return SubscriptionType(value: "Weekly", color: .blue)
+        case .threeMonth:
+            return SubscriptionType(value: "3-Months", color: .orange)
+        }
+    }
+}
