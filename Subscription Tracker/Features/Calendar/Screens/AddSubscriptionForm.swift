81fd7c158badb57245e42c4a14cf8b56f4fb9488
Nikita Shyshkin <yatoenough@gmail.com>
2024-12-19 23:50:30 +0100

    Implemented subsciptions adding

--- Subscription Tracker/Features/Calendar/Screens/AddSubscriptionForm.swift
+++ Subscription Tracker/Features/Calendar/Screens/AddSubscriptionForm.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct AddSubscriptionForm: View {
     @State private var name: String = ""
     
     @State private var date: Date = Date()
-    @State private var amount: String = ""
+    @State private var amount: Double?
     
     private var subscriptionTypes: [SubscriptionType] {
         var types: [SubscriptionType] = []
@@ -21,7 +22,11 @@ struct AddSubscriptionForm: View {
         }
         return types
     }
+    
     @State private var frequency: SubscriptionType = DefaultSubscriptionTypes.monthly.getValue()
+    @State private var alertVisible = false
+    
+    @Environment(SubscriptionsViewModel.self) var subscriptionsViewModel: SubscriptionsViewModel
     
     var body: some View {
         
@@ -46,30 +51,62 @@ struct AddSubscriptionForm: View {
                     HStack {
                         Text("$")
                         
-                        TextField("0.00", text: $amount)
+                        TextField("0.00", value: $amount, formatter: numberFormatter())
                             .keyboardType(.decimalPad)
                     }
-                    
-                    
                 }
+                
+                Button("Add Subscription") { addSubscription() }
+                #warning("Fix keyboard dismiss")
             }
-            .onTapGesture {
-                dismissKeyboard()
-            }
+//            .onTapGesture {
+//                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+//            }
             .navigationTitle("Add Subscription")
             .scrollContentBackground(.hidden)
         }
+        .alert("Validation Error", isPresented: $alertVisible) {
+            Button("OK", role: .cancel) { }
+        }
     }
     
-    private func dismissKeyboard() {
-        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    private func addSubscription() {
+        let isDataValid = validateData()
+        
+        if !isDataValid {
+            alertVisible = true
+            return
+        }
+        
+        let subscription = Subscription(name: name, price: amount!, type: frequency, date: date)
+        subscriptionsViewModel.addSubscription(subscription)
+    }
+    
+    private func numberFormatter() -> NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    private func validateData() -> Bool {
+        guard amount != nil else { return false }
+        guard !name.isEmpty else { return false }
+        
+        return true
     }
     
 }
 
 #Preview {
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: Subscription.self, SubscriptionType.self, configurations: config)
+    
     NavigationView {
         AddSubscriptionForm()
-    }.preferredColorScheme(.dark)
+    }
+    .preferredColorScheme(.dark)
+    .modelContainer(container)
+    .environment(SubscriptionsViewModel(modelContext: container.mainContext))
     
 }
